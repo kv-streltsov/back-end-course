0297c876d74c2d4af856728021ecbd7af990a070
kv.streltsov <kv.streltsov@yandex.ru>
2023-04-26 18:06:15 +0500

    test

--- dist/db/db.js
+++ dist/db/db.js
@@ -1,26 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.video_list = void 0;
-const interface_video_1 = require("../dto/interface.video");
 exports.video_list = [
-    {
-        id: 1,
-        title: 'snowboard',
-        author: 'kv.streltsov',
-        canBeDownloaded: false,
-        minAgeRestriction: 18,
-        createdAt: '2023-04-25T03:39:35.504Z',
-        publicationDate: '2023-04-26T03:39:35.504Z',
-        availableResolutions: interface_video_1.Resolutions.P1080
-    },
-    {
-        id: 2,
-        title: '01 - Back-end Путь Самурая',
-        author: 'IT-KAMASUTRA',
-        canBeDownloaded: false,
-        minAgeRestriction: 12,
-        createdAt: '2023-04-10T03:39:35.504Z',
-        publicationDate: '2023-04-11T03:39:35.504Z',
-        availableResolutions: interface_video_1.Resolutions.P720
-    }
+// {
+// 	id:1,
+// 	title: 'snowboard',
+// 	author: 'kv.streltsov',
+// 	canBeDownloaded: false,
+// 	minAgeRestriction:18,
+// 	createdAt: '2023-04-25T03:39:35.504Z',
+// 	publicationDate:'2023-04-26T03:39:35.504Z',
+// 	availableResolutions: Resolutions.P1080
+// },
+// {
+// 	id:2,
+// 	title: '01 - Back-end Путь Самурая',
+// 	author: 'IT-KAMASUTRA',
+// 	canBeDownloaded: false,
+// 	minAgeRestriction:12,
+// 	createdAt: '2023-04-10T03:39:35.504Z',
+// 	publicationDate:'2023-04-11T03:39:35.504Z',
+// 	availableResolutions: Resolutions.P720
+// }
 ];
