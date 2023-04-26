1d0deaf8330e2340ef477f829bfcb2d3bbdac317
kv.streltsov <kv.streltsov@yandex.ru>
2023-04-26 17:39:22 +0500

    test

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,24 +1,5 @@
 import {InterfaceVideo, Resolutions} from "../dto/interface.video";
 
 export let video_list: InterfaceVideo[] = [
-	{
-		id:1,
-		title: 'snowboard',
-		author: 'kv.streltsov',
-		canBeDownloaded: false,
-		minAgeRestriction:18,
-		createdAt: '2023-04-25T03:39:35.504Z',
-		publicationDate:'2023-04-26T03:39:35.504Z',
-		availableResolutions: Resolutions.P1080
-	},
-	{
-		id:2,
-		title: '01 - Back-end Путь Самурая',
-		author: 'IT-KAMASUTRA',
-		canBeDownloaded: false,
-		minAgeRestriction:12,
-		createdAt: '2023-04-10T03:39:35.504Z',
-		publicationDate:'2023-04-11T03:39:35.504Z',
-		availableResolutions: Resolutions.P720
-	}
+
 ]
