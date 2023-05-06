ba8fd691be65f4ae4fe7b6299cd80d05e596ebf2
kv-streltsov <kv.streltsov@yandex.ru>
2023-05-06 19:28:56 +0700

    test valid

--- dist/middleware/validation/posts-validation.js
+++ dist/middleware/validation/posts-validation.js
@@ -7,14 +7,20 @@ const db_1 = require("../../db/db");
 const titleValidation = (0, express_validator_1.body)('title').isString().trim().notEmpty().isLength({ max: 30 });
 const shortDescriptionValidation = (0, express_validator_1.body)('shortDescription').isString().trim().notEmpty().isLength({ max: 100 });
 const contentValidation = (0, express_validator_1.body)('content').isString().trim().notEmpty().isLength({ max: 1000 });
-const blogIdValidation = (0, express_validator_1.body)('blogId').isString().trim().notEmpty()
-    .custom(blogId => {
-    const findBlogId = db_1.posts_list.findIndex(value => value.blogId === blogId);
-    if (findBlogId !== -1) {
-        throw new Error('blogId already in use');
+const blogIdValidation = (0, express_validator_1.body)('blogId').isString().trim().notEmpty().custom(blogId => {
+    const checkBlogId = db_1.blogs_list.findIndex(value => value.id === blogId);
+    if (checkBlogId) {
+        throw new Error('id not found in blog');
     }
-    // return true
+    return true;
 });
+// .custom(blogId => {
+//
+//     const findPostId: number = posts_list.findIndex(value => value.blogId === blogId)
+//     if (findPostId !== -1) {throw new Error('blogId already in use')}
+//     return true
+//
+// })
 exports.createPostValidation = [titleValidation, shortDescriptionValidation, contentValidation, blogIdValidation, input_validation_middleware_1.inputValidationMiddleware];
 exports.updatePostValidation = [titleValidation, shortDescriptionValidation, contentValidation, input_validation_middleware_1.inputValidationMiddleware];
 //# sourceMappingURL=posts-validation.js.map
--- src/middleware/validation/posts-validation.ts
+++ src/middleware/validation/posts-validation.ts
@@ -6,15 +6,23 @@ const titleValidation = body('title').isString().trim().notEmpty().isLength({max
 const shortDescriptionValidation = body('shortDescription').isString().trim().notEmpty().isLength({max: 100})
 const contentValidation = body('content').isString().trim().notEmpty().isLength({max: 1000})
 
-const blogIdValidation = body('blogId').isString().trim().notEmpty()
-    .custom(blogId => {
-        const findBlogId: number = posts_list.findIndex(value => value.blogId === blogId)
+const blogIdValidation = body('blogId').isString().trim().notEmpty().custom(blogId =>{
+
+    const checkBlogId:number = blogs_list.findIndex(value => value.id === blogId)
+    if(checkBlogId){throw new Error('id not found in blog')}
+    return true
+
+})
 
 
+    // .custom(blogId => {
+    //
+    //     const findPostId: number = posts_list.findIndex(value => value.blogId === blogId)
+    //     if (findPostId !== -1) {throw new Error('blogId already in use')}
+    //     return true
+    //
+    // })
 
-        if (findBlogId !== -1) {throw new Error('blogId already in use')}
-        // return true
-    })
 export const createPostValidation =
     [titleValidation, shortDescriptionValidation, contentValidation, blogIdValidation, inputValidationMiddleware]
 export const updatePostValidation =
