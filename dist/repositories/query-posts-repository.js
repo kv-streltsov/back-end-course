9e07e12e99ecac227654d16fac2d9c5e04123a59
kv-streltsov <kv.streltsov@yandex.ru>
2023-05-20 14:31:56 +0700

    fix getAllPost

--- dist/repositories/query-posts-repository.js
+++ dist/repositories/query-posts-repository.js
@@ -12,13 +12,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.queryPostsRepository = void 0;
 const db_mongo_1 = require("../db/db_mongo");
 exports.queryPostsRepository = {
-    getAllPosts: (pageNumber = 1, pageSize = 10, sortBy = 'createAt', sortDirection = 'desc') => __awaiter(void 0, void 0, void 0, function* () {
+    getAllPosts: (pageNumber = 1, pageSize = 11, sortBy = 'createAt', sortDirection = 'desc') => __awaiter(void 0, void 0, void 0, function* () {
+        console.log(pageSize);
         const count = yield db_mongo_1.collectionPosts.countDocuments({});
         const posts = yield db_mongo_1.collectionPosts.find({}, { projection: { _id: 0 } })
             .skip((pageNumber - 1) * pageSize)
             .sort(sortDirection)
             .limit(pageSize)
             .toArray();
+        console.log();
         return {
             pagesCount: Math.ceil(count / pageSize),
             page: pageNumber,
--- src/repositories/query-posts-repository.ts
+++ src/repositories/query-posts-repository.ts
@@ -2,15 +2,16 @@ import {collectionPosts} from "../db/db_mongo";
 
 export const queryPostsRepository = {
 
-    getAllPosts: async (pageNumber: number = 1, pageSize: number = 10, sortBy: string = 'createAt',sortDirection:string = 'desc') => {
+    getAllPosts: async (pageNumber: number = 1, pageSize: number = 11, sortBy: string = 'createAt',sortDirection:string = 'desc') => {
 
+        console.log(pageSize)
         const count = await collectionPosts.countDocuments({})
         const posts = await collectionPosts.find({},{projection: {_id: 0}})
             .skip((pageNumber - 1) * pageSize)
             .sort(sortDirection)
             .limit(pageSize)
             .toArray()
-
+        console.log()
         return {
             pagesCount: Math.ceil(count / pageSize),
             page: pageNumber,
